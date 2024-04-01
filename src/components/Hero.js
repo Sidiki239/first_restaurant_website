0336f22ea4591ac20b7bb4ad09d91fc3cbdb118f
Sidiki239 <143208350+Sidiki239@users.noreply.github.com>
2024-04-01 15:10:20 +0000

    Update button didn't works but now dooes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,52 +11,62 @@ function Hero() {
 const navigate = useNavigate()
   return (
     <>
+    <div className='w-full'>
+ 
     <div className=' relative '>
   
       <img
 className='w-full
- lg:h-400 right-0 top-0  '      
+ lg:h-400  right-0 top-0  '      
       src={burger} alt='burgerImage'
       
       />
 
 
-<div className=' sm:h-[calc(100vh-120px)] lg:absolute   z-10  lg:items-start  sm:pt-0 top-0 '>
-  <div className='space-y-4 bg-[#fffff98] z-10 p-4 b-10 sm:top-10  lg:text-left text-center '><br/><br/><br/>
+<div className=' sm:h-[calc(100vh-120px)] lg:absolute   sm: mt-10 sm:items-center   z-10  lg:items-start   sm:pt-5 top-10 '>
+  <div className='space-y-4 bg-[#fffff98] z-10 p-4 b-10 sm:top-5  lg:text-left text-center '><br/>
 {/* <p className='uppercase font-medium text-white  sm:mt-50 '>Wide option of choice</p> */}
 <h2 className='text-5xl lg:text-7xl lg:ml-5 sm:text-6xl lg:text-white dark:text-white sm:text-black font-bold lg:text-left text-center'>
 Delicious <span className='text-accent '>Food</span>
 </h2>
-<p className='text-gray-700 lg:ml-5 lg:text-[18px]
+<p  className='text-gray-700 lg:ml-5 lg:text-[18px]
 sm:text-[16px] text-center lg:text-left 
 '>Delicious food color , aroma  and taste<br/>
 Commander tout de suite !!
-</p><br/>
-{width > 500 &&
-<a href='/menu' className='lg:ml-5 lg:t-5 sm:text-center z-10 justify-center lg:justify-start flex lg:text-left'>
-<button 
-  className='bg-accent text-white px-4 py-2 text-2xl  rounded-3xl sm:text-3xl  lg:text-4xl  lg:px-4 lg:py-2 cursor-pointer shadow-4xl'
+</p>
+
+
+
+
+
+
+
+</div>
+<div style={{textAlign: width < 500 ? "center": "inherit" }}  >
+<a    href='/menu' className='lg:ml-5 lg:t-10  sm:w-full   z-10 relative     lg:text-left'>
+<button  style={{textAlign:"center",alignSelf:"center",}} 
+  className='bg-accent text-white px-4 py-2 text-2xl  lg:rounded-5xl rounded-3xl sm:text-3xl  lg:text-5xl lg:ml-4 lg:mt-6 lg:px-8 lg:py-2 cursor-pointer shadow-4xl'
   onClick={()=>navigate("menu")}
 >
  Explorer
-</button></a>  
+</button></a> 
+</div> 
 
-}
-</div>
 </div>
 
     </div>
-   {width < 500 &&<div   onClick={()=>navigate("/menu")} className=' w-auto justify-center' style={{textAlign:"center",justifyContent:"center",zIndex:5}}><a href='/menu' className='z-20  '>
-<button 
-  className='bg-accent text-white px-4 py-2 text-2xl  rounded-3xl sm:text-3xl  lg:text-4xl  lg:px-4 lg:py-2 cursor-pointer shadow-4xl'
-  onClick={()=>navigate("/menu")}
->
- Explorer
-</button></a></div>  } 
-
-    <br/><br/><br/>
+     
+  
     
+  
+    </div>
+ 
+ 
+
+   
+    <br/><br/> <br/><br/> <br/>
     </>
+
   )
 }
 
